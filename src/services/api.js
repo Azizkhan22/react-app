666716ed18b19e400ecc8361560a154673a9dbe1
Azizkhan22 <azizkhan22kakar@gmail.com>
2025-08-13 21:04:41 +0500

    dev: change the api hardcoded to env variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
-const API_BASE_URL = 'http://localhost:8000/api';
-
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+console.log(API_BASE_URL)
 
 function getCookie(name) {
-  const value = `; ${document.cookie}`;
+  const value = `; ${document.cookie}`; 
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(';').shift();
 }
